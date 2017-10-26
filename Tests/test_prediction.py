563139e2eb830f21d7303b02e80473daeef9de89
Your Name <you@example.com>
2017-10-26 03:12:54 +0200

    message

--- Tests/test_prediction.py
+++ Tests/test_prediction.py
@@ -1,12 +1,10 @@
 import os
 import numpy as np
 import sys
-project_path = "/Users/pantelisvlachas/Documents/PhD/journal_club_travis/LSTM_Reduced"
-sys.path.insert(0, project_path)
 
 # directory where project is located
 #project_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir)) + "/LSTM_Reduced"
-
+project_path = os.getcwd() + "/LSTM_Reduced"
 
 
 
@@ -40,6 +38,7 @@ def test_capital_case():
 
 def test_prediction():
     with cd(project_path):
+        sys.path.insert(0, project_path)
         from prediction import main as perform_test_prediction
         prediction_output = perform_test_prediction()
     assert np.sum(prediction_output[:10,0,0] - [-2.85755336, -2.42378372, -2.02809878, -1.66715797, -1.33677999, -1.03306286, -0.75441993, -0.49803106, -0.2612441 , -0.04188509])<1e-6
